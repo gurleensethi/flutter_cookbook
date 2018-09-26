e78ebbe3c26e7969d508a7a00378cd4bfe3569da
Gurleen Sethi <sarusethi@rocketmail.com>
2018-09-26 11:12:10 +0530

    Update code for theme and snackbars

--- lib/design_basics/displaying_snackbars/displaying_snackbars_code.dart
+++ lib/design_basics/displaying_snackbars/displaying_snackbars_code.dart
@@ -1,14 +1,11 @@
-final snackbar = """<code class=" language-dart"><span class="token keyword"><span class="kd">final</span></span> <span class="n">snackBar</span> <span class="token operator"><span class="o">=</span></span> <span class="token function"><span class="n">SnackBar</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="nl">content<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">Text</span></span><span class="token punctuation"><span class="o">(</span></span><span class="token string"><span class="s">'Yay! A SnackBar!'</span></span><span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="nl">action<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">SnackBarAction</span></span><span class="token punctuation"><span class="o">(</span></span>
-    <span class="nl">label<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token string"><span class="s">'Undo'</span></span><span class="token punctuation"><span class="o">,</span></span>
-    <span class="nl">onPressed<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token punctuation"></span><span class="o"><span class="token punctuation">(</span><span class="token punctuation">)</span></span><span class="token punctuation"></span> <span class="token punctuation"><span class="o">{</span></span>
-      <span class="token comment" spellcheck="true"><span class="c1">// Some code to undo the change!</span></span>
-    <span class="token punctuation"></span><span class="o"><span class="token punctuation">}</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-<span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>
-<code class=" language-dart"><span class="token comment" spellcheck="true"><span class="c1">// Find the Scaffold in the Widget tree and use it to show a SnackBar</span></span>
-<span class="n">Scaffold</span><span class="token punctuation"><span class="o">.</span></span><span class="token function"><span class="na">of</span></span><span class="token punctuation"><span class="o">(</span></span><span class="n">context</span><span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">.</span></span><span class="token punctuation"></span><span class="token function"><span class="na">showSnackBar</span></span><span class="token punctuation"><span class="o">(</span></span><span class="n">snackBar</span><span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>
-""";
+final snackbar = """final snackBar = SnackBar(
+  content: Text('Yay! A SnackBar!'),
+  action: SnackBarAction(
+    label: 'Undo',
+    onPressed: () {
+      // Some code to undo the change!
+    },
+  ),
+);
+
+Scaffold.of(context).showSnackBar(snackBar);""";
--- lib/design_basics/using_themes/using_themes_code.dart
+++ lib/design_basics/using_themes/using_themes_code.dart
@@ -1,24 +1,22 @@
 final code = {
   "theme":
-      """<code class=" language-dart"><span class="token function"><span class="n">Theme</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="token comment" spellcheck="true"><span class="c1">// Create a unique theme with "ThemeData"</span></span>
-  <span class="nl">data<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">ThemeData</span></span><span class="token punctuation"><span class="o">(</span></span>
-    <span class="nl">accentColor<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="n">Colors</span><span class="token punctuation"><span class="o">.</span></span><span class="na">yellow</span><span class="token punctuation"><span class="o">,</span></span>
-  <span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="nl">child<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">FloatingActionButton</span></span><span class="token punctuation"><span class="o">(</span></span>
-    <span class="nl">onPressed<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token punctuation"></span><span class="o"><span class="token punctuation">(</span><span class="token punctuation">)</span></span><span class="token punctuation"></span> <span class="token punctuation"></span><span class="o"><span class="token punctuation">{</span><span class="token punctuation">}</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-    <span class="nl">child<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">Icon</span></span><span class="token punctuation"><span class="o">(</span></span><span class="n">Icons</span><span class="token punctuation"><span class="o">.</span></span><span class="na">add</span><span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-<span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>""",
+      """Theme(
+  // Create a unique theme with "ThemeData"
+  data: ThemeData(
+    accentColor: Colors.yellow,
+  ),
+  child: FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+  ),
+);""",
   "main_theme":
-      """<code class=" language-dart"><span class="token function"><span class="n">MaterialApp</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="nl">title<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="n">title</span><span class="token punctuation"><span class="o">,</span></span>
-  <span class="nl">theme<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">ThemeData</span></span><span class="token punctuation"><span class="o">(</span></span>
-    <span class="nl">brightness<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="n">Brightness</span><span class="token punctuation"><span class="o">.</span></span><span class="na">dark</span><span class="token punctuation"><span class="o">,</span></span>
-    <span class="nl">primaryColor<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="n">Colors</span><span class="token punctuation"><span class="o">.</span></span><span class="na">lightBlue</span><span class="token punctuation"><span class="o">[</span></span><span class="token number"><span class="mi">800</span></span><span class="token punctuation"></span><span class="o"><span class="token punctuation">]</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-    <span class="nl">accentColor<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="n">Colors</span><span class="token punctuation"><span class="o">.</span></span><span class="na">cyan</span><span class="token punctuation"><span class="o">[</span></span><span class="token number"><span class="mi">600</span></span><span class="token punctuation"></span><span class="o"><span class="token punctuation">]</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-<span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>"""
+      """MaterialApp(
+  title: title,
+  theme: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.lightBlue[800],
+    accentColor: Colors.cyan[600],
+  ),
+);"""
 };
