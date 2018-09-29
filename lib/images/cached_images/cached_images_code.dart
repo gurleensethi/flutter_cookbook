3cc381728c4cbafc6bf9bcfe957eadffbe5ccd84
Gurleen Sethi <sarusethi@rocketmail.com>
2018-09-29 10:28:36 +0530

    Convert html code to text code for images section.

--- lib/images/cached_images/cached_images_code.dart
+++ lib/images/cached_images/cached_images_code.dart
@@ -1,18 +1,8 @@
-final cachedNetworkImagePackage = """
-<p>cached_network_image</p>
-""";
+final cachedNetworkImagePackage = """cached_network_image""";
 
-final cachedNetworkImage = """
-<code class=" language-dart"><span class="token function"><span class="n">CachedNetworkImage</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="nl">imageUrl<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token string"><span class="s">'image url'</span></span><span class="token punctuation"><span class="o">,</span></span>
-<span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>
-""";
+final cachedNetworkImage = """CachedNetworkImage( imageUrl: 'image url', );""";
 
-final cachedNetworkImagePlaceholder = """
-<code class=" language-dart"><span class="token function"><span class="n">CachedNetworkImage</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="nl">placeholder<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token function"><span class="n">CircularProgressIndicator</span></span><span class="token punctuation"></span><span class="o"><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">,</span></span><span class="token punctuation"></span>
-  <span class="nl">imageUrl<span class="token punctuation">:</span></span><span class="token punctuation"></span> <span class="token string"><span class="s">'image url'</span></span><span class="token punctuation"><span class="o">,</span></span>
-<span class="token punctuation"></span><span class="o"><span class="token punctuation">)</span><span class="token punctuation">;</span></span><span class="token punctuation"></span>
-</code>
-""";
+final cachedNetworkImagePlaceholder = """CachedNetworkImage(
+  placeholder: CircularProgressIndicator(),
+  imageUrl: 'image url',
+);""";
--- lib/images/images_from_internet/images_from_internet_code.dart
+++ lib/images/images_from_internet/images_from_internet_code.dart
@@ -1,13 +1,3 @@
-final image = """
-<code class=" language-dart"><span class="n">Image</span><span class="token punctuation"><span class="o">.</span></span><span class="token function"><span class="na">network</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="token string"><span class="s">'image url'</span></span><span class="token punctuation"><span class="o">,</span></span>
-<span class="token punctuation"><span class="o">)</span></span>
-</code>
-""";
+final image = """Image.network( 'image url', )""";
 
-final gif = """
-<code class=" language-dart"><span class="n">Image</span><span class="token punctuation"><span class="o">.</span></span><span class="token function"><span class="na">network</span></span><span class="token punctuation"><span class="o">(</span></span>
-  <span class="token string"><span class="s">'gif url'</span></span><span class="token punctuation"><span class="o">,</span></span>
-<span class="token punctuation"><span class="o">)</span></span>
-</code>
-""";
+final gif = """Image.network( 'gif url', )""";
